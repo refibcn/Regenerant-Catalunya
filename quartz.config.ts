904623135888afb17649ddcc05e430606d451c34
Luiz Fernando <luizfernandolfsg@gmail.com>
2025-10-11 14:10:46 -0300

    Update CNAME and baseUrl for Regenerant Catalunya subdomain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "refibcn.cat",
+    baseUrl: "regenerant-catalunya.refibcn.cat",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
